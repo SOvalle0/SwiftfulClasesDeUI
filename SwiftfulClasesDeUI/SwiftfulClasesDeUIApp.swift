2c9fbfd29d2aa4f9f90a57f66f71415b11914d9e
Sebastian O <sovalle2000@gmail.com>
2024-06-20 20:50:29 -0400

    Initial Commit

---
+++ SwiftfulClasesDeUI/SwiftfulClasesDeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulClasesDeUIApp.swift
+//  SwiftfulClasesDeUI
+//
+//  Created by Macbook Air on 20-06-24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulClasesDeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
